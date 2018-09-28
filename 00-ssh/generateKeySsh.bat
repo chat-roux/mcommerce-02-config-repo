0907f2fcbb9482bab26d1694f55f139a3cdd8484
chat_roux <tdalgalian@hotmail.com>
2018-09-28 13:22:52 +0200

    Modification du batch de commandes ssh

--- 00-ssh/generateKeySsh.bat
+++ 00-ssh/generateKeySsh.bat
@@ -1,9 +1,12 @@
 ################################################################"
 # (01.)GENERATE RSA-SSH-KEY
 ################################################################"
-ssh-keygen -t rsa -b 4096 -C "tcharou.dalgalian@afpa.fr"
+ssh-keygen -t rsa^
+           -b 4096^
+		   -C "tcharou.dalgalian@afpa.fr"
 
 ################################################################"
 # (02.)GET THE HOST-KEY OF THE HOST 'git@github.com'
 ################################################################"
-ssh -T git@github.com
+ssh -T^
+     git@github.com
